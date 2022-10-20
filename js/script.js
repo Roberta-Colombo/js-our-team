e5121c3f45bd14646b357beb0fdabf5bb5f1c819
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-20 14:56:39 +0200

    Creato array di oggetti

---
+++ js/script.js
@@ -0,0 +1,64 @@
+/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+MILESTONE 1:
+Stampare su console le informazioni di nome, ruolo e la stringa della foto
+MILESTONE 2:
+Stampare le stesse informazioni su DOM sottoforma di stringhe
+BONUS 1:
+Trasformare la stringa foto in una immagine effettiva
+BONUS 2:
+Organizzare i singoli membri in card/schede e stilarli a vostro gusto :leichtes_lächeln:
+BONUS 3:
+Aggiungere attraverso un form un membro al team */
+
+// creo array con dati oggetti:
+
+const team = [
+    {
+       'name': 'Wayne',
+       'last_name': 'Barnett',
+       'role': 'Founder &amp; CEO',
+       'image': 'wayne.jpg'
+    },
+
+    {
+        'name': 'Angela',
+        'last_name': 'Caroll',
+        'role': 'Chief Editor',
+        'image': 'carol.jpg'
+    },
+
+    {
+        'name': 'Walter',
+        'last_name': 'Gordon',
+        'role': 'Office Manager',
+        'image': 'walter.jpg'
+    },
+
+    {
+        'name': 'Angela',
+        'last_name': 'Lopez',
+        'role': 'Social Media Manager',
+        'image': 'angela.jpg'
+    },
+
+    {
+        'name': 'Scott',
+        'last_name': 'Estrada',
+        'role': 'Developer',
+        'image': 'scott.jpg'
+
+    },
+
+    {
+        'name': 'Barbara',
+        'last_name': 'Ramos',
+        'role': 'Graphic Designer',
+        'image': 'barbara.jpg'
+    }
+
+]
+
+console.log(team);
