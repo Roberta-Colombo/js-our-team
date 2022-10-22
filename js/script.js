b7fcdd0f98b38b0aea4aa9b39d3fb9314547f35e
Roberta-Colombo <roberta.colombo89@hotmail.it>
2022-10-22 13:54:22 +0200

    Corretta creazione nuovo membro tramite form

--- js/script.js
+++ js/script.js
@@ -1,116 +1,113 @@
-/* Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
-Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console le informazioni di nome, ruolo e la stringa della foto
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede e stilarli a vostro gusto :leichtes_lächeln:
-BONUS 3:
-Aggiungere attraverso un form un membro al team */
-
 // creo array con dati oggetti:
 
 const team = [
     {
-       'name': 'Wayne',
-       'last_name': 'Barnett',
-       'role': 'Founder &amp; CEO',
-       'image': './img/wayne-barnett-founder-ceo.jpg'
+       name: 'Wayne',
+       last_name: 'Barnett',
+       role: 'Founder &amp; CEO',
+       image: 'wayne-barnett-founder-ceo.jpg'
     },
 
     {
-        'name': 'Angela',
-        'last_name': 'Caroll',
-        'role': 'Chief Editor',
-        'image': './img/angela-caroll-chief-editor.jpg'
+        name: 'Angela',
+        last_name: 'Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
     },
 
     {
-        'name': 'Walter',
-        'last_name': 'Gordon',
-        'role': 'Office Manager',
-        'image': './img/walter-gordon-office-manager.jpg'
+        name: 'Walter',
+        last_name: 'Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
     },
 
     {
-        'name': 'Angela',
-        'last_name': 'Lopez',
-        'role': 'Social Media Manager',
-        'image': './img/angela-lopez-social-media-manager.jpg'
+        name: 'Angela',
+        last_name: 'Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
     },
 
     {
-        'name': 'Scott',
-        'last_name': 'Estrada',
-        'role': 'Developer',
-        'image': './img/scott-estrada-developer.jpg'
+        name: 'Scott',
+        last_name: 'Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
 
     },
 
     {
-        'name': 'Barbara',
-        'last_name': 'Ramos',
-        'role': 'Graphic Designer',
-        'image': './img/barbara-ramos-graphic-designer.jpg'
+        name: 'Barbara',
+        last_name: 'Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
     }
 
 ]
 console.log(team);
 
-// stampo dati di ogni membro in console e poi in DOM + aggiungo foto:
-
+// creo row dove posizionare i div dei singoli membri:
 const container = document.querySelector('.container');
 
-for(i in team){
-    console.log(team[i]);
+function createAllCards(){
+    const row = document.createElement('div');
+    row.className = 'row';
+    for(let i = 0; i < team.length; i++){
+        console.log(team[i]);
+        const member = team[i];
+        const template = printOneCard(member);
+        row.innerHTML += template;   
+    }    
+}
+createAllCards();
 
-    const infoDiv = document.createElement('div');
-    container.append(infoDiv);
-    const member = team[i];
-    infoDiv.innerHTML = "<div class='text-style'>" +  
-    member.name + " " + member.last_name + "<br>" + "<em>"+member.role+"</em>" + "</div>" + "<br>" + "<img class='img-properties' src="+ member.image+ ">"
+// funzione per creare un singolo div (card membro con nome + ruolo + foto):
+function printOneCard(member){
+        const infoDiv = document.createElement('div');
+        container.append(infoDiv);
+        infoDiv.innerHTML = "<div class='text-style'>" +  
+        member.name + " " + member.last_name + "<br>" + "<em>" + member.role +"</em>" + "</div>" + "<br>" + "<img class='img-properties' src=./img/" + member.image + ">"
+
+        return infoDiv;
 }
 
 const addBtn = document.getElementById('add-btn');
 const submitBtn = document.getElementById('submit');
 
+//funzione per aggiungere il form al click sul button "add member":
 function addForm(){
-    // const fieldName = document.createElement('input');
-    // const fieldLastName = document.createElement('input');
-    // const fieldRole = document.createElement('input');
-    // const fieldimage = document.createElement('input');
-
-    // const sectionNewMember = document.querySelector('.new-member-section');
-    // sectionNewMember.append(fieldName);
-    // sectionNewMember.append(fieldLastName);
-    // sectionNewMember.append(fieldRole);
-    // sectionNewMember.append(fieldimage);
     const newForm = document.getElementById('new-member-form').classList.remove("d-none");
     newForm.classList.add("show");
 }
 
+//funzione per raccogliere i dati dal form e generare una nuova card membro:
 function collectData(){
-    const newMemberData = [];
 
-    const newName = document.getElementById('field-name').value;
-    newMemberData.push(newName);
-    const newLastName = document.getElementById('field-last-name').value;
-    newMemberData.push(newLastName);
-    const newRole = document.getElementById('field-role').value;
-    newMemberData.push(newRole);
-    const newImage = document.getElementById('field-image').value;
-    newMemberData.push(newImage);
+    let newName = document.getElementById('field-name').value;
+    let newLastName = document.getElementById('field-last-name').value;
+    let newRole = document.getElementById('field-role').value;
+    let newImage = document.getElementById('field-image').value;
 
-    console.log(newMemberData);       
+    const newMember = {
+        name: newName,
+        last_name: newLastName,
+        role: newRole,
+        image: newImage
+    }
+    team.push(newMember);
+    console.log(newMember); 
+    const newInfoDiv = printOneCard(newMember);
+    container.append(newInfoDiv); 
+   
+    document.getElementById('field-name').value = '';
+    document.getElementById('field-last-name').value = '';
+    document.getElementById('field-role').value = '';
+    document.getElementById('field-image').value = '';
 }
-
 
 addBtn.addEventListener('click', addForm);
 
 submitBtn.addEventListener('click', collectData);
+
 
